5e201db8a751bb9ace34ca5c11b12019017164c4
VICNAWXU <ext-maaru.turunen@traficom.fi>
2024-12-23 10:26:16 +0200

    testing

---
+++ .github/workflows/hash.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+console.log("Reading files from 'dist'...");
+const files = fs.readdirSync("dist");
+console.log("Files found:", files);
+
+const results = files.map(file => {
+    const hash = crypto.createHash("sha256").update(fs.readFileSync(`dist/${file}`)).digest("hex");
+    return `${file}: ${hash}`;
+});
+
+console.log("Hashes generated:", results);
+
+fs.writeFileSync("dist/sha256sum.txt", results.join("\n"));
+console.log("Hashes saved to dist/sha256sum.txt");
