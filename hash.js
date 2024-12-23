c3f1808e3c9b907b7a5e607db208515e6e2507ed
VICNAWXU <ext-maaru.turunen@traficom.fi>
2024-12-23 10:50:25 +0200

    test 4

--- hash.js
+++ hash.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
 
-console.log("Reading files from 'dist'...");
-const files = fs.readdirSync("dist");
+console.log("Reading files...");
+const files = fs.readdirSync("out/prod/make/");
 console.log("Files found:", files);
 
 const results = files.map(file => {
@@ -12,5 +12,5 @@ const results = files.map(file => {
 
 console.log("Hashes generated:", results);
 
-fs.writeFileSync("dist/sha256sum.txt", results.join("\n"));
-console.log("Hashes saved to dist/sha256sum.txt");
+fs.writeFileSync("out/prod/make/sha256sum.txt", results.join("\n"));
+console.log("Hashes saved to sha256sum.txt");
